7f4a61f372062b2e8bc9bea9a2009db6aff058ac
suned <sune.debel@medable.com>
2022-03-22 12:11:26 +0100

    chore: add home-manager config

---
+++ nixpkgs/home.nix
@@ -0,0 +1,94 @@
+{ config, pkgs, lib, ... }:
+let
+  sources = import ../nix/sources.nix {};
+  unstable = import sources.nixpkgs { config = { allowUnfree = true; }; };
+in
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "dev";
+  home.homeDirectory = "/Users/dev";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  nixpkgs.config.allowUnfree = true;
+  
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  
+  home.packages = [
+    unstable.iterm2
+    unstable.vscode
+    unstable.fira-code
+    unstable._1password
+    unstable.niv
+  ];
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+  
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+      }
+    ];
+    shellAbbrs = {
+      # git
+      gst = "git status";
+      ga = "git add";
+      gaa = "git add -A";
+      gcm = "git commit -m";
+      gp = "git push";
+      gco = "git checkout";
+
+      # home manager
+      hms = "home-manager switch";
+      hme = "home-manager edit";
+    };
+    shellInit = ''
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+      end
+
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+      end
+      
+      starship init fish | source
+      eval (direnv hook fish)
+      set -Ux EDITOR code
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "suned";
+    userEmail = "sune.debel@medable.com";
+  };
+}
